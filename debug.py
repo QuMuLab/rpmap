f9dc08359cb8c7dfda39173098c1c9a19c4ed226
beckydvn <18rldv@queensu.ca>
2025-08-18 21:51:50 -0400

    add back original pdkbddl files for debugging

--- debug.py
+++ debug.py
@@ -1,4 +1,6 @@
 from pdkb.planner import solve, cleanup
 
-solve("bdi_testing/bdi_pdkbddl_files/bdi_mvex_problem.pdkbddl")
+# solve("bdi_testing/bdi_pdkbddl_files/bdi_mvex_problem.pdkbddl")
+solve("bdi_testing/original_pdkbddl_files/problem.pdkbddl")
+
 cleanup()
