fe1fbbf214fd623d93b3d696716af4ea346225b8
beckydvn <18rldv@queensu.ca>
2025-11-27 16:28:04 -0500

    create single solve function

--- evaluation_bdi-grapevine.py
+++ evaluation_bdi-grapevine.py
@@ -1,4 +1,4 @@
 from evaluation_template import evaluate
 
 if __name__ == "__main__":
-    evaluate("bdi-grapevine")
+    evaluate("bdi-grapevine", 3, 4, 3)
--- evaluation_template.py
+++ evaluation_template.py
@@ -15,58 +15,63 @@ DEPTH = [2, 3]
 def get_agents_str(num_agents):
     return f"\t(:agents {' '.join(['alice', 'bob', 'cindy', 'derek', 'evelyn'][:num_agents])})"
 
-def eval_solve(dom, parser):
+def solve_single(dom, problem, num_agents, dep, parser):
+    base_path = os.path.join("bdi_extension", dom)
+    domain_path = os.path.join(base_path, "domain.pdkbddl")
+    with open(domain_path) as f:
+        lines = f.readlines()
+
+    with open(domain_path, "w") as f:
+        for line in lines:
+            if line.lstrip().startswith("(:agents"):
+                f.write(get_agents_str(num_agents) + "\n") 
+            elif line.lstrip().startswith("(:depth"):
+                f.write(f"(:depth {dep})")
+            else:
+                f.write(line)
+
+    # add the correct number of agents to the domain
+    t0 = time.time()
+    # grab the PDDL
+    problem_name = f"problem_{problem}" 
+    pddl_str = "\n".join(read_pdkbddl_file(os.path.join(base_path, f"{problem_name}.pdkbddl")))
+    
+    result = parser(pddl_str)
+    grounded_dom_path = os.path.join(base_path, "pdkb-domain.pddl")
+    grounded_prob_path = os.path.join(base_path, "pdkb-problem.pddl")
+    anc_effs, domain, problem = (result[0].children, *ground(result[1], result[2], grounded_dom_path))
+    num_fluents_before_pre = len(domain.predicates)
+    print("Applying conditional effects...")
+    domain, problem = apply_cond_effs(anc_effs, domain, problem)
+    print("Done preprocessing!")
+    num_fluents_after_pre = len(domain.predicates)
+    pddl.core.Domain.grounded_print = True
+    pddl.core.Action.grounded_print = True
+    write(grounded_dom_path, str(domain))
+    write(grounded_prob_path, str(problem))
+    preprocessing_time = time.time() - t0
+    print("Solving...")
+    plan_length, solve_time = solve(base_path)
+    with open("evaluation.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows([[dom, problem.name, num_agents, dep, problem >= 2, num_fluents_before_pre, num_fluents_after_pre, preprocessing_time, solve_time, plan_length]])
+
+    if plan_length == 0:
+        print("No plan found!")
+        return
+
+def eval_solve_domain(dom, parser):
     for i in range(4):
         for dep in DEPTH:   
             problem = i + 1 # iterating through [0-3], problem files are [1-4]
             num_agents = i + 2 # iterating through [0-3], number of agents is [2-5]
             print(f"Domain: {dom}\nProblem: {problem}")
-            
-            base_path = os.path.join("bdi_extension", dom)
-            domain_path = os.path.join(base_path, "domain.pdkbddl")
-            with open(domain_path) as f:
-                lines = f.readlines()
-
-            with open(domain_path, "w") as f:
-                for line in lines:
-                    if line.lstrip().startswith("(:agents"):
-                        f.write(get_agents_str(num_agents) + "\n") 
-                    elif line.lstrip().startswith("(:depth"):
-                        f.write(f"(:depth {dep})")
-                    else:
-                        f.write(line)
-
-            # add the correct number of agents to the domain
-            t0 = time.time()
-            # grab the PDDL
-            problem_name = f"problem_{problem}" 
-            pddl_str = "\n".join(read_pdkbddl_file(os.path.join(base_path, f"{problem_name}.pdkbddl")))
-            
-            result = parser(pddl_str)
-            grounded_dom_path = os.path.join(base_path, "pdkb-domain.pddl")
-            grounded_prob_path = os.path.join(base_path, "pdkb-problem.pddl")
-            anc_effs, domain, problem = (result[0].children, *ground(result[1], result[2], grounded_dom_path))
-            num_fluents_before_pre = len(domain.predicates)
-            print("Applying conditional effects...")
-            domain, problem = apply_cond_effs(anc_effs, domain, problem)
-            print("Done preprocessing!")
-            num_fluents_after_pre = len(domain.predicates)
-            pddl.core.Domain.grounded_print = True
-            pddl.core.Action.grounded_print = True
-            write(grounded_dom_path, str(domain))
-            write(grounded_prob_path, str(problem))
-            preprocessing_time = time.time() - t0
-            print("Solving...")
-            plan_length, solve_time = solve(base_path)
-            with open("evaluation.csv", "a", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerows([[dom, problem.name, num_agents, dep, i >= 2, num_fluents_before_pre, num_fluents_after_pre, preprocessing_time, solve_time, plan_length]])
-
-            if plan_length == 0:
-                print("No plan found!")
+            if not solve_single(dom, problem, num_agents, dep):
                 break
+            
+            
 
-def evaluate(domain):
+def evaluate(domain, prob=None, num_agents=None, dep=None):
     # --- GENERAL PARSING SETUP ---
     # read the ancillary effects grammar file and add to the main grammar file
     with open("bdi_extension/ancillary_effects.lark", "r") as f:
@@ -86,4 +91,7 @@ def evaluate(domain):
         writer.writerows([["Domain Name", "Problem Name", "Number of Agents", "Depth", "Requires Depth 3", "Number of Fluents before Preprocessing", "Number of Fluents after Preprocessing", "Preprocessing Time", "Solve Time", "Plan Length"]])
 
     # --- MAIN EVALUATION BODY ---
-    eval_solve(domain, parser)
+    if prob and num_agents and dep:
+        solve_single(domain, prob, num_agents, dep, parser)
+    else:
+        eval_solve_domain(domain, parser)
