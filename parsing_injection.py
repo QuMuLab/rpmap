51b3555da45d5ce8b618e1e62ac68bb536786cd9
beckydvn <18rldv@queensu.ca>
2025-11-30 20:20:21 -0500

    run belief intention domain

--- parsing_injection.py
+++ parsing_injection.py
@@ -394,8 +394,8 @@ if __name__ == "__main__":
     construct_domain_grammar()
     construct_problem_grammar()
     # grab the PDDL
-    base_path = "bdi_extension/bdi-grapevine"
-    pddl_str = "\n".join(read_pdkbddl_file(f"{base_path}/problem_1.pdkbddl"))
+    base_path = "bdi_extension/belief-intention"
+    pddl_str = "\n".join(read_pdkbddl_file(f"{base_path}/problem_6.pdkbddl"))
     # read the lark file
     with open(GRAMMAR_FILE, "r") as f:
         grammar = f.read()
