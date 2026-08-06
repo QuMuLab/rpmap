590100063a2077d0a125f9045b6d424fb73c98f7
beckydvn <18rldv@queensu.ca>
2026-08-06 16:32:31 -0400

    ground actions (and ensure intentions can only be applied to predicates that match action names)

--- refactored_rpmap/parsing_and_grounding/ground.py
+++ refactored_rpmap/parsing_and_grounding/ground.py
@@ -1,120 +1,98 @@
 from .parsing_utils import create_valuations
-from .core.anc_eff import Agent, ActionMODLType, PossibleActionMODLType
+from .core.anc_eff import ActionMODLType, PossibleActionMODLType, MODL, Agent
 from copy import deepcopy
 from pddl.action import Action
 from pddl.logic.base import And, Not, ForallCondition
 from pddl.logic.effects import Forall, When
 from pddl.logic.terms import Constant
-from pddl.logic.predicates import Predicate, ForallCondition
+from pddl.logic.predicates import Predicate
 
 
-def ground_formula(domain, problem, formula, assignment):
-    if type(formula) is Predicate:
-        return predicates_to_fluents([formula], assignment, domain, problem)[0]
-    elif type(formula) is Not:
-        p = ground_formula(domain, problem, formula.argument, assignment)
-        if type(p) is Predicate:
-            p.negated = True
-            return p
-        else:
-            raise NotImplementedError("Deal with a complex Not formula?")
-    elif type(formula) is And:
-        return And(*[ground_formula(domain, problem, o, assignment)  for o in formula.operands])
-    elif type(formula) is Forall:
-        # need to get all values for this variable
-        grounded = []
-        var_names = [v.name for v in formula.variables]
-        val_generator = create_valuations(domain._agents, problem.objects, formula.variables)
-        for valuation in val_generator:
-            # need to add onto the existing assignment so we retain knowledge of outer variables
-            for var_name, val in zip(var_names, valuation):
-                assignment[var_name] = val
-            grounded.append(ground_formula(domain, problem, formula.effect, assignment))
-        return And(*grounded)
-    elif type(formula) is When:
-        cond = ground_formula(domain, problem, formula.condition, assignment)
-        if type(cond) is Predicate:
-            # for formatting reasons we want to force this into being an "And"
-            and_term = And(*[])
-            and_term._operands.append(cond)
-            cond = and_term
-        return When(cond, ground_formula(domain, problem, formula.effect, assignment))
-
-def check_intention_error(f: Predicate, domain):
-    # TODO: fix this. instead of getting the deepest child, iterate to see if we find an intention MODL (would only be at the second-last place).
-    # also, we need to update the MODL class so we can work with the assumption that there is always a terminal RML.
-    # technically this should be guaranteed through the grammar, but we want to guarantee this for manual construction of MODLs too.
+def check_intention_error(modl: MODL, domain):
     action_names = [a.name for a in domain.actions]
-    if isinstance(f.get_deepest_child(), ActionMODLType) or isinstance(f.get_deepest_child(), PossibleActionMODLType) and f.get_deepest_child().name not in action_names:
-        raise ValueError("Cannot intend a predicate; you can only intend an action.")
+    current = deepcopy(modl)
+    while isinstance(current.child, MODL):
+        current = deepcopy(current.child)
+    if isinstance(current.mod_type, ActionMODLType) or isinstance(current.mod_type, PossibleActionMODLType):
+        if current.child.name not in action_names:
+            raise ValueError("Cannot intend a predicate; you can only intend an action.")
 
-def predicates_to_fluents(predicates: list[Predicate], assignment, domain, problem):
+def ground_formula(formula, assignment, domain, problem):
     fluents = []
-    for p in predicates:
-        p = deepcopy(p)
-        new_terms = []
+    for fo in formula:
+        fo = deepcopy(fo)
+        if isinstance(fo, Predicate):
+            fl = Predicate(fo.name, *[Constant(assignment[t.name]) for t in fo.terms])
+            fl.negated = fo.negated
+            # find the "always known" status by referencing it from the domain predicates
+            fl.always_known = False
+            for dp in domain.predicates:
+                if dp.name == fl.name and len(dp.terms) == len(fl.terms):
+                    fl.always_known = dp.always_known
+                    break
+            fluents.append(fl)
         # change to isinstance
-        if type(p) is ForallCondition:
-            vars = {v for v in p.variables}
+        elif isinstance(fo, ForallCondition):
+            vars = {v for v in fo.variables}
             val_generator = create_valuations(domain._agents, problem.objects, vars)
             for valuation in val_generator:
                 var_names = [v.name for v in vars]
                 for var_name, val in zip(var_names, valuation):
                     assignment[var_name] = val
-                fluents.extend(predicates_to_fluents([p.condition], assignment, domain, problem))
-            return fluents
-        elif type(p) is And:
-            for o in p.operands:
-                fluents.extend(predicates_to_fluents([o], assignment, domain, problem) )
-            return fluents
-        elif type(p) is Predicate:
-            terms = p.terms
-        elif type(p) is MODL:
-            print()
-        elif hasattr(p, "argument"):
-            outside_formula_type = type(p)
-            terms = p.argument.terms
-        else:
-            raise ValueError("Unknown predicate type?")
-        for t in terms:
-            new_terms.append(Constant(assignment[t.name]))
-        
-        if type(p) is not Predicate:
-            f = Predicate(p.argument.name, *new_terms)
-            f.negated = p.argument.negated
-            f.always_known = p.argument.always_known
-            f = outside_formula_type(f)
-        else:
-            f = Predicate(p.name, *new_terms)
-            if p.modl:  
-                if p.modl.agent.var:
-                    p.modl.agent = Agent(assignment[p.modl.agent.name], False)   
-                if p.modl.nested:
-                    for i in range(len(p.modl.nested)):
-                        p.modl.nested[i].agent = Agent(assignment[p.modl.nested[i].agent.name], False)
-            f.modl = p.modl
-            f.negated = (p.negated == True) # become False if it's None
-            # find the "always known" status by referencing it from the domain predicates
-            f.always_known = False
-            for dp in domain.predicates:
-                if dp.name == f.name and len(dp.terms) == len(f.terms):
-                    f.always_known = dp.always_known
-                    break
-            if not f.always_known and f.negated:
-                f.negated = False
-                if f.modl:
-                    if f.modl.nested:
-                        f.modl.nested[-1].negate_inner_rml = not p.modl.nested[-1].negate_inner_rml
-                    else:
-                        f.modl.negate_inner_rml = not f.modl.negate_inner_rml
-                else:
-                    pass
-                    # f.modl = NegateOnly(True)
-        if f.modl:
-            check_intention_error(f, domain)
-        fluents.append(f)
-    return fluents
+                fluents.extend(ground_formula([fo.condition], assignment, domain, problem))
+        elif isinstance(fo, Forall):
+            grounded = []
+            var_names = [v.name for v in fo.variables]
+            val_generator = create_valuations(domain._agents, problem.objects, fo.variables)
+            for valuation in val_generator:
+                # need to add onto the existing assignment so we retain knowledge of outer variables
+                for var_name, val in zip(var_names, valuation):
+                    assignment[var_name] = val
+                grounded.append(ground_formula([fo.effect], assignment, domain, problem))
+            return And(*grounded)
+        elif isinstance(fo, And):
+            fluents.extend([ground_formula([o], assignment, domain, problem) for o in fo.operands])
+        elif isinstance(fo, Not):
+            fl = ground_formula([fo.argument], assignment, domain, problem)
+            fl.negated = True
+            fluents.append(fl)
+        elif hasattr(fo, "argument"):
+            fluents.append(ground_formula([fo.argument], assignment, domain, problem))
+        elif isinstance(fo, MODL):
+            # need to set the base predicate of the MODL to the grounded predicate
+            # first, convert the MODL to a list of MODLs with no children with the new grounded predicate at the end
+            modls = []
+            current = deepcopy(fo)
+            while isinstance(current, MODL):
+                current_no_child = deepcopy(current)
+                current_no_child.child = None
+                current_no_child.agent = Agent(assignment[current_no_child.agent.name], False)
+                modls.append(current_no_child)
+                current = deepcopy(current.child)
+            modls.append(ground_formula([fo.get_deepest_child()], assignment, domain, problem)[0])
+            for i in range(len(modls) - 1, - 1, - 1):
+                if isinstance(modls[i], Predicate):
+                    continue
+                modls[i] = modls[i](modls[i + 1])
+            grounded_modl = modls[0]
+            check_intention_error(grounded_modl, domain)
+            fluents.append(grounded_modl)
+        elif isinstance(fo, When):
+            cond = ground_formula([fo.condition], assignment, domain, problem)
+            # for formatting reasons we want to force this into being an "And"
+            and_term = And(*[])
+            and_term._operands.extend(cond)
+            cond = and_term
 
+            eff = ground_formula([fo.effect], assignment, domain, problem)
+            and_term = And(*[])
+            and_term._operands.extend(eff)
+            eff = and_term
+            return When(cond, eff)
+        else:
+            raise ValueError("Unknown formula type: " + str(type(fo)))
+    return fluents
+        
 def create_base_operators(domain, problem):
     operators = set()
 
@@ -125,33 +103,29 @@ def create_base_operators(domain, problem):
             assignment = {var_name: val for var_name, val in zip(var_names, valuation)}
             op_name_suffix = "_".join([assignment[var.name] for var in a.parameters])
             op_name = a.name + "_" + op_name_suffix if op_name_suffix else a.name
-            # TODO: handle other types of formulas?
             pass_pre = a.precondition.operands if type(a.precondition) is And else [a.precondition]
-            precondition = And(*predicates_to_fluents(pass_pre, assignment, domain, problem))
+            precondition = And(*ground_formula(pass_pre, assignment, domain, problem))
+
+            if not isinstance(precondition, And):
+                and_ = And(*[])
+                and_._operands.append(precondition)
+                precondition = and_
+
+            effect = ground_formula([a.effect], assignment, domain, problem) 
+
+            if not isinstance(effect, And):
+                and_ = And(*[])
+                and_._operands.append(effect)
+                effect = and_
+
+            new_a = Action(
+                    op_name,
+                    None,
+                    precondition,
+                    effect
+                )
 
             print()
-    #         and_ = And(*[])
-    #         if type(precondition) is And:
-    #             and_._operands.extend(precondition._operands)
-    #         else:
-    #             and_._operands.append(precondition)
-    #         precondition = and_
-
-    #         effect = ground_formula(domain, problem, a.effect, assignment) 
-            
-    #         and_ = And(*[])
-    #         if type(effect) is And:
-    #             and_._operands.extend(effect._operands)
-    #         else:
-    #             and_._operands.append(effect)
-    #         effect = and_
-
-    #         new_a = Action(
-    #                 op_name,
-    #                 None,
-    #                 precondition,
-    #                 effect
-    #             )
     #         new_a.assignment = assignment
     #         if type(a.derive_condition) is list:
     #             # have a complex derived condition
